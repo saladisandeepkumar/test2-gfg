8c7875b523a3606250585e31ef8ff757f36a108b
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2024-08-18 10:07:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split an array into two equal Sum subarrays/split-an-array-into-two-equal-sum-subarrays.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canSplit(self, arr):
+        #code here
+        s,c=sum(arr),0
+        for i in arr:
+            c+=i
+            s-=i
+            if c==s:
+                return True
+        return False
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().splitlines()
+
+    t = int(data[0])
+    index = 1
+    for _ in range(t):
+        arr = list(map(int, data[index].split()))
+        index += 1
+
+        obj = Solution()
+        res = obj.canSplit(arr)
+        if (res):
+            print("true")
+        else:
+            print("false")
+
+# } Driver Code Ends
