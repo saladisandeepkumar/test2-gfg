69e5b3f5556cb23db5c0ac46cc4eb8c1f17c9e7a
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2024-09-15 11:11:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Second Largest/second-largest.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+class Solution:
+    def print2largest(self, arr):
+        # Code Here
+        arr=list(set(arr))
+        arr.sort(reverse=True)
+        if len(arr)>1:
+            return arr[1]
+        else:
+            return -1
+        
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        ans = ob.print2largest(arr)
+        print(ans)
+
+# } Driver Code Ends
