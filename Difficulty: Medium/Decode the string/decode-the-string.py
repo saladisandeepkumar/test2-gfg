f99ce10e0709ecb36284ddab1f263fcd5a683051
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2025-09-17 22:58:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Decode the string/decode-the-string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def decodedString(self, s):
+        # code here
+        d_s=[] #decodedString
+        for i in s:
+            if i=="]":
+                sub_string=""
+                while d_s[-1] != '[':
+                    sub_string = d_s.pop() + sub_string #prepend the character
+                d_s.pop()
+                
+                repitation=''
+                while d_s and d_s[-1].isdigit():
+                    repitation = d_s.pop() + repitation 
+                
+                d_s.append(int(repitation) * sub_string)
+                continue
+            
+            d_s.append(i)
+        
+        return ''.join(d_s)
