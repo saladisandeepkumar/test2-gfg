5c1afad8b3bb5fddaee858267c88ea7ae8e5ffa5
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2025-12-15 20:04:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Indices to Balance Even and Odd Sums/count-indices-to-balance-even-and-odd-sums.py
@@ -0,0 +1,13 @@
+class Solution:
+    def cntWays(self, arr):
+        # code here
+        arr = [arr[i] if i%2 == 0 else -arr[i] for i in range(len(arr))]
+        sols = []
+        ls = 0
+        s = sum(arr)
+        for i in range(len(arr)):
+            s = s - arr[i]
+            if  ls - s == 0:
+                sols.append(i)
+            ls = ls + arr[i]
+        return len(sols)
