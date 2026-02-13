db3e5bb21a25930fa5d8909846cb9181336ec82c
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2026-02-13 19:46:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/All numbers with specific difference/all-numbers-with-specific-difference.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getCount(self, n, d):
+        # code here 
+        if n <= d:
+            return 0
+        lo, hi = 1, n + 1
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if mid - sum(map(int, str(mid))) >= d:
+                hi = mid
+            else:
+                lo = mid + 1
+        return n - lo + 1
+
+
+        
+        
