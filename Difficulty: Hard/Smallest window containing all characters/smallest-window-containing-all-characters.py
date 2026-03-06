19f2bf90f02aaf974454994fca8834b2cbbdffc1
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2026-03-06 21:39:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.py
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.py
@@ -1,31 +1,24 @@
 class Solution:
-    def smallestWindow(self, s, p):
+    def minWindow(self, s, p):
         # code here
-        from collections import Counter
-        
-        need = Counter(p)
-        need_count = len(need)  
-        have = 0
-        window = {}
-        
-        res, res_len = [-1, -1], float("inf")
-        left = 0
-        for right, ch in enumerate(s):
-            window[ch] = window.get(ch, 0) + 1
-
-            if ch in need and window[ch] == need[ch]:
-                have += 1
-
-            while have == need_count:
-                if (right - left + 1) < res_len:
-                    res = [left, right]
-                    res_len = right - left + 1
-
-                window[s[left]] -= 1
-                if s[left] in need and window[s[left]] < need[s[left]]:
-                    have -= 1
-                left += 1
-
-        l, r = res
-        return s[l:r+1] if res_len != float("inf") else ""
-        
+        def check(a,b):
+            for i in b:
+                if i not in a or b[i]>a[i]:
+                    return False
+            return True
+        h={}
+        b={}
+        for i in p:
+            b[i]=b.get(i,0)+1
+        l=0
+        r=""
+        v=""
+        for i in range(len(s)):
+            v+=s[i]
+            h[s[i]]=h.get(s[i],0)+1
+            while check(h,b):
+                if  i-l+1<len(r) or len(r)==0:
+                    r=s[l:i+1]
+                h[s[l]]-=1
+                l+=1
+        return r
