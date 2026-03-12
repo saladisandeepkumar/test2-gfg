89b1ac8c46336001fef612c8b6078b61cc5c0d86
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2026-03-12 21:19:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.py
+++ Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.py
@@ -1,20 +1,21 @@
 class Solution:
     def kBitFlips(self, arr, k):
-        # code here
         n = len(arr)
-        is_flipped = [0] * n
-        flip_count = 0
-        curr_flip = 0
+        flipped = [0] * n
+        flip = 0
+        ans = 0
 
         for i in range(n):
+
             if i >= k:
-                curr_flip ^= is_flipped[i - k]
-            if arr[i] ^ curr_flip == 0:
+                flip ^= flipped[i-k]
+
+            if arr[i] ^ flip == 0:
                 if i + k > n:
                     return -1
-                flip_count += 1
-                curr_flip ^= 1
-                is_flipped[i] = 1
 
-        return flip_count
-        
+                ans += 1
+                flip ^= 1
+                flipped[i] = 1
+
+        return ans
